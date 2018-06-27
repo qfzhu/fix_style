e8fef8397760573b406b661d5df3740dc9215678
zhuqingfu <qfzhu@ir.hit.edu.cn>
2018-06-27 20:22:38 +0800

    update sample

--- sample.py
+++ sample.py
@@ -6,8 +6,9 @@ from torch.autograd import Variable
 from torchvision import transforms
 from build_vocab import Vocab
 from data_loader import Rescale
-from models import EncoderCNN
+from models import EncoderRNN
 from models import FactoredLSTM
+from data_loader import get_data_loader
 
 
 def to_var(x, volatile=False):
@@ -33,31 +34,43 @@ def main():
         vocab = pickle.load(f)
 
     # build model
-    encoder = EncoderCNN(300)
+    encoder = EncoderRNN(voc_size=60376, emb_size=300, hidden_size=300)
     decoder = FactoredLSTM(300, 512, 512, len(vocab))
 
     encoder.load_state_dict(torch.load('pretrained_models/encoder-15.pkl'))
     decoder.load_state_dict(torch.load('pretrained_models/decoder-15.pkl'))
 
     # prepare images
-    transform = transforms.Compose([
-        Rescale((224, 224)),
-        transforms.ToTensor()
-        ])
-    img_names, img_list = load_sample_images('sample_images/', transform)
-    image = to_var(img_list[30], volatile=True)
+    # transform = transforms.Compose([
+    #     Rescale((224, 224)),
+    #     transforms.ToTensor()
+    #     ])
+    # img_names, img_list = load_sample_images('sample_images/', transform)
+    # image = to_var(img_list[30], volatile=True)
+
+    data_loader = get_data_loader('', 'data/factual_train.txt.m', vocab, 1)
 
     # if torch.cuda.is_available():
     #     encoder = encoder.cuda()
     #     decoder = decoder.cuda()
 
-    # farward
-    features = encoder(image)
-    output = decoder.sample(features, mode="factual")
+    for i, (messages, m_lengths, targets, t_lengths) in enumerate(data_loader):
+        messages = to_var(messages.long())
+        targets = to_var(targets.long())
 
-    caption = [vocab.i2w[x] for x in output]
-    print(img_names[30])
-    print(caption)
+        # forward, backward and optimize
+        output, features = encoder(messages, list(m_lengths))
+        outputs = decoder.sample(features, mode="factual")
+        caption = [vocab.i2w[x] for x in output]
+        print caption
+
+    # farward
+    # features = encoder(image)
+    # output = decoder.sample(features, mode="factual")
+    #
+    # caption = [vocab.i2w[x] for x in output]
+    # print(img_names[30])
+    # print(caption)
 
 
 if __name__ == '__main__':
